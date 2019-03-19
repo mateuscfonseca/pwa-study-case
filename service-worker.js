b6b6a392c3ff412e64c16a4eaec6b5e270a9e5c5
Mateus <mateuscfonseca@gmail.com>
2019-03-19 11:22:41 -0300

    getting info from cache when offline

--- service-worker.js
+++ service-worker.js
@@ -5,6 +5,12 @@
 // You may obtain a copy of the License at
 // 
 //      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 var dataCacheName = 'dataCache';
 var cacheName = 'cacheExemple';
@@ -40,4 +46,9 @@ self.addEventListener('activate', function(e) {
 
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request).catch(e => console.log(e));
+      })
+    );
 });
